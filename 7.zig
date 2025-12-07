ccdeeac54392bbc50267e5e61f27935ca9a707f9
Philip Linde <philip@text.garden>
2025-12-07 19:04:22 +0100

    Improve solution 7

--- 7.zig
+++ 7.zig
@@ -2,77 +2,56 @@ const std = @import("std");
 
 const input = @embedFile("inputs/7");
 
-pub fn main() anyerror!void {
-    std.debug.print("{}\n{}\n", .{ solution1(), solution2() });
-}
-
-fn solution1() usize {
-    var mut_input: [input.len]u8 = undefined;
-    std.mem.copyForwards(u8, &mut_input, input);
-
-    var splits: usize = 0;
-
-    var prev_line: ?[]const u8 = null;
-    var line_iter = std.mem.splitScalar(u8, &mut_input, '\n');
-    while (line_iter.next()) |line| {
-        if (line.len == 0) break;
-        defer prev_line = line;
-        const prev = prev_line orelse continue;
-
-        for (0..line.len) |ux| {
-            const x: isize = @intCast(ux);
-            const left_split = get(line, x - 1) == '^' and get(prev, x - 1) == '|';
-            const right_split = get(line, x + 1) == '^' and get(prev, x + 1) == '|';
-            const continue_beam = get(line, x) == '.' and get(prev, x) == '|';
-            const below_start = get(prev, x) == 'S';
-
-            if (left_split or right_split or continue_beam or below_start) {
-                @constCast(line)[ux] = '|';
-            }
-
-            if (get(line, x) == '^' and get(prev, x) == '|') splits += 1;
-        }
-    }
-    return splits;
-}
-
-inline fn get(line: []const u8, x: isize) u8 {
-    return if (x < 0 or x >= line.len) '.' else line[@intCast(x)];
-}
-
+const cell_visited = std.math.maxInt(usize) - 3;
 const cell_split = std.math.maxInt(usize) - 2;
 const cell_empty = std.math.maxInt(usize) - 1;
 const cell_linebreak = std.math.maxInt(usize);
 
-fn solution2() usize {
-    var dynamic_input: [input.len]usize = undefined;
+const Iter = std.mem.SplitIterator(usize, .scalar);
+const State = std.meta.Tuple(&.{ Iter, usize });
 
-    var start_x: usize = 0;
-    for (input, 0..) |b, i| {
-        switch (b) {
-            '\n' => dynamic_input[i] = cell_linebreak,
-            '.' => dynamic_input[i] = cell_empty,
-            '^' => dynamic_input[i] = cell_split,
-            'S' => {
-                dynamic_input[i] = cell_empty;
-                start_x = i;
-            },
-            else => unreachable,
-        }
-    }
+pub fn main() anyerror!void {
+    var tmp: [input.len]usize = undefined;
 
-    const iter = std.mem.splitScalar(usize, &dynamic_input, cell_linebreak);
-    return 1 + timelines(start_x, iter);
+    std.debug.print("{}\n{}\n", .{ p1(genMap(&tmp)), p2(genMap(&tmp)) });
 }
 
-const DynamicIterator = std.mem.SplitIterator(usize, .scalar);
+fn genMap(out: *[input.len]usize) State {
+    var start_x: usize = 0;
+    for (input, 0..) |b, i| switch (b) {
+        '\n' => out[i] = cell_linebreak,
+        '.' => out[i] = cell_empty,
+        '^' => out[i] = cell_split,
+        'S' => {
+            out[i] = cell_empty;
+            start_x = i;
+        },
+        else => unreachable,
+    };
+    return .{ std.mem.splitScalar(usize, out, cell_linebreak), start_x };
+}
 
-fn timelines(x: usize, iter_const: DynamicIterator) usize {
-    var iter = iter_const;
+fn p1(state: State) usize {
+    var iter = state.@"0";
+    const x = state.@"1";
     while (iter.next()) |line| {
         if (line.len == 0) return 0;
+        if (line[x] == cell_visited) return 0;
         if (line[x] == cell_split) {
-            const count = 1 + timelines(x - 1, iter) + timelines(x + 1, iter);
+            @constCast(line)[x] = cell_visited;
+            return 1 + p1(.{ iter, x - 1 }) + p1(.{ iter, x + 1 });
+        }
+    }
+    unreachable;
+}
+
+fn p2(state: State) usize {
+    var iter = state.@"0";
+    const x = state.@"1";
+    while (iter.next()) |line| {
+        if (line.len == 0) return 1;
+        if (line[x] == cell_split) {
+            const count = p2(.{ iter, x + 1 }) + p2(.{ iter, x - 1 });
             @constCast(line)[x] = count;
             return count;
         }
